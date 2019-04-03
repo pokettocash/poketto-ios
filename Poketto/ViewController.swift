00ff7f522e26f70d95d0ef43b675510389e41605
Tiago Alves <alvesjtiago@gmail.com>
2019-04-03 18:22:24 +0100

    Initial Commit

---
+++ Poketto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Poketto
+//
+//  Created by Tiago Alves on 03/04/2019.
+//  Copyright © 2019 Poketto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
