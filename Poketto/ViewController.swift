f9dbb843adbae565159d7d24f75d2d15ca4dcf63
Tiago Alves <alvesjtiago@gmail.com>
2019-04-11 08:25:27 +0100

    Send transaction on xDai POA network

--- Poketto/ViewController.swift
+++ Poketto/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 import SwiftKeychainWrapper
 import Web3swift
+import EthereumABI
+import EthereumAddress
+import BigInt
 
 class ViewController: UIViewController {
 
@@ -19,10 +22,14 @@ class ViewController: UIViewController {
         // let newWallet = generateWallet()
         // print(newWallet?.addresses?.first)
         
-        let explorer = Explorer.init()
-        explorer.balanceFrom(address: "0xDc6789592EEDe0789b7a04E703c7D05244737e86", completion: { balance in
-            print(balance)
-        })
+        // Get wallet balance
+        // let explorer = Explorer.init()
+        // explorer.balanceFrom(address: "0x569d656393ca2e1b62a362a6a60556b2ad56721d", completion: { balance in
+            // print(balance)
+        // })
+        
+        // Execute transaction on xDai chain
+        // send(toAddress: "0x394a29F426F6505d40854ABb730D1c8DE29C8C87", value: "0.001")
     }
     
     func generateWallet() -> AbstractKeystore? {
@@ -31,8 +38,45 @@ class ViewController: UIViewController {
         
         // Store mnemonic on keychain
         KeychainWrapper.standard.set(mnemonic, forKey: "mnemonic")
+        print(mnemonic)
         
         return keystore
+    }
+    
+    func send(toAddress: String, value: String) {
+        let toAddress = EthereumAddress(toAddress)!
+        
+        let endpoint = "https://dai.poa.network"
+        let connection = web3(provider: Web3HttpProvider(URL(string: endpoint)!)!)
+        
+        let mnemonic = KeychainWrapper.standard.string(forKey: "mnemonic")
+        let keystore = try! BIP32Keystore(mnemonics: mnemonic!)
+        let ownWalletAddress = keystore?.addresses?.first!
+        
+        let keystoreManager = KeystoreManager.init([keystore!])
+        connection.addKeystoreManager(keystoreManager)
+        
+        let contract = connection.contract(Web3.Utils.coldWalletABI, at: toAddress, abiVersion: 2)!
+        let amount = Web3.Utils.parseToBigUInt(value, units: .eth)
+        var options = TransactionOptions.defaultOptions
+        options.value = amount
+        options.from = ownWalletAddress
+        options.gasPrice = .automatic
+        options.gasLimit = .automatic
+        let tx = contract.write(
+            "fallback",
+            parameters: [AnyObject](),
+            extraData: Data(),
+            transactionOptions: options)!
+        
+        let result = try! tx.send()
+        
+        // Balance
+        // let balanceResult = try! connection.eth.getBalance(address: walletAddress)
+        // let balanceString = Web3.Utils.formatToEthereumUnits(balanceResult, toUnits: .eth, decimals: 6)!
+        // print(balanceString)
+        
+        print(result)
     }
 
 }
